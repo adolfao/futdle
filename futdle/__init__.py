7a58709c2073c746eb55095b0346bb88492189db
Adolfo <adolfogabriel1@hotmail.com>
2025-07-22 01:38:46 -0300

    imagens home feita e projeto saindo do papel!!!!!

--- futdle/__init__.py
+++ futdle/__init__.py
@@ -2,4 +2,6 @@ from flask import Flask
 
 def create_app():
   app = Flask(__name__)
+  from futdle.routes import main
+  app.register_blueprint(main)
   return app
---
+++ futdle/routes.py
@@ -0,0 +1,8 @@
+from futdle import create_app
+from flask import render_template, Blueprint
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def home():
+  return render_template('home.html')
